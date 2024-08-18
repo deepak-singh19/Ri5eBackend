72b6b1ff9c056dfe979065e9de784103a1a63d1e
Parth Rathod <parth.rathod12@gmail.com>
2024-08-18 16:22:43 +0530

    updated message to fix product find

--- routers/message.js
+++ routers/message.js
@@ -5,21 +5,22 @@ const Conversation = require("../database/models/conversation-model");
 const Mentor = require("../database/models/mentor-model");
 const Product = require("../database/models/product-model");
 const Message = require("../database/models/message-model");
+const { default: mongoose } = require('mongoose');
 
 router.post("/conversation", async (req, res) => {
     try {
         // Corrected the request body access
         // console.lo(req);
         const { senderId, recieverId } = req.body;
-        console.log(senderId, recieverId);
+        // console.log("ids from frontend ", senderId, recieverId);
 
         // Check if the conversation exists
-        const existingConversation = await Conversation.findOne({ members: [senderId, recieverId] });
-        console.log(existingConversation);
+        const existingConversation = await Conversation.findOne({ member: [senderId, recieverId] });
+        // console.log(existingConversation);
 
         if (!existingConversation) {
             // If not found, create a new conversation
-            const newConversation = new Conversation({ members: [senderId, recieverId] });
+            const newConversation = new Conversation({ member: [senderId, recieverId] });
             await newConversation.save();
             return res.status(200).send({ message: "Conversation created successfully" });
         }
@@ -110,17 +111,24 @@ router.get("/product/conversation/:userId", async (req, res) => {
     try {
         const userId = req.params.userId;
 
-        console.log(userId);
 
-        const conversations = await Conversation.find({ members: { $in: [userId] } });
+        // ---------------
+        // const conversations = await Conversation.find({ _id: (userId)})
 
-        console.log(conversations);
+        // ---------------
+        const userObjectId = new mongoose.Types.ObjectId(userId);
+        console.log(userObjectId);
+
+        const conversations = await Conversation.find({ member: { $in: [userObjectId] } });
+
+        console.log("find conversation using user id", conversations);
 
         const conversationUserData = await Promise.all(conversations.map(async (conv) => {
-            const recieverId = conv.members.find((mem) => mem !== userId);
-            console.log(recieverId);
-            const product = await Product.findById(recieverId);
-            console.log(product);
+            const recieverId = conv.member.find((mem) => mem !== userId);
+            // const recieverObjectId = new mongoose.Types.ObjectId(recieverId);
+            // console.log(recieverObjectId);
+            const product = await Product.findOne({_id: recieverId});
+            console.log("product from receiver", product);
             return { product: { email: product.email, fullName: product.fullName }, conversationId: conv._id };
         }));
 
