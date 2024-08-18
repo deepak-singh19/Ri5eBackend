54de8de82b69bd56eec5573e0e44f351db717e10
deepak-singh19 <deepaksingh23lodhi@gmail.com>
2024-08-18 14:37:38 +0530

    Added

--- routers/message.js
+++ routers/message.js
@@ -1,138 +1,128 @@
 const express = require('express');
 const router = express.Router();
 
+const Conversation = require("../database/models/conversation-model");
+const Mentor = require("../database/models/mentor-model");
+const Product = require("../database/models/product-model");
+const Message = require("../database/models/message-model");
 
-const Conversation= require("../database/models/conversation-model");
-const Mentor= require("../database/models/mentor-model");
-const Product= require("../database/models/product-model");
-const Message= require("../database/models/message-model");
-
-
-router.post("/conversation",async(req,res)=>{
-    try{
-
-        console.log(req);
-        const {senderId, recieverId}= req.body.body;
+router.post("/conversation", async (req, res) => {
+    try {
+        // Corrected the request body access
+        const { senderId, recieverId } = req.body.body;
         console.log(senderId, recieverId);
 
-        const existingConversation= await Conversation.findOne({member:[senderId,recieverId]});
+        // Check if the conversation exists
+        const existingConversation = await Conversation.findOne({ members: [senderId, recieverId] });
         console.log(existingConversation);
-        
-        if(!existingConversation){
-            
-            const newConversation= new Conversation({member:[senderId,recieverId]});
+
+        if (!existingConversation) {
+            // If not found, create a new conversation
+            const newConversation = new Conversation({ members: [senderId, recieverId] });
             await newConversation.save();
-            res.status(200).send({message:"Conversation created successfully"})
-                 
+            return res.status(200).send({ message: "Conversation created successfully" });
         }
-        return res.status(409).send({message:"Conversation already exists"})   
-        // const newConversation= new Conversation({member:[senderId,recieverId]});
-        // await Conversation.save();
-        // res.status(200).send({message:"Conversation created successfully"})
 
-    }catch(error){
+        // If conversation exists, send conflict status
+        return res.status(409).send({ message: "Conversation already exists" });
+
+    } catch (error) {
         console.log(error);
-        res.send(500).send({error:"Server error. Please try again."});
+        return res.status(500).send({ error: "Server error. Please try again." });
     }
-})
+});
 
-router.get("/mentor/conversation/:userId", async(req, res)=>{
-    try{
+router.get("/mentor/conversation/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
 
-        const userId= req.params.userId;
+        const conversations = await Conversation.find({ members: { $in: [userId] } });
 
-        const conversation= await Conversation({member:{$in:[userId]}})
-
-        const converstionUserData= Promise.all(conversation.map(async(conv)=>{
-            const recieverId= conv.member.find((mem)=> mem!==userId);
-            const mentor= await Mentor.findById(recieverId);
-            return { mentor: {email:mentor.email, fullName:mentor.fullName}, conversationId:conv._id}
+        const conversationUserData = await Promise.all(conversations.map(async (conv) => {
+            const recieverId = conv.members.find((mem) => mem !== userId);
+            const mentor = await Mentor.findById(recieverId);
+            return { mentor: { email: mentor.email, fullName: mentor.fullName }, conversationId: conv._id };
         }));
 
+        res.status(200).send({ message: "All conversation", conversationData: conversationUserData });
 
-        res.status(200).send({message:"All conversation", conversationData:converstionUserData})
-
-    }catch(error){
+    } catch (error) {
         console.log(error);
-        res.send(500).send({error:"Server error. Please try again."});
+        return res.status(500).send({ error: "Server error. Please try again." });
     }
-})
+});
 
-router.post ("/message", async(req, res)=>{
-    try{
-        const {conversationId, senderId, message, recieverId=''}= req.body;
-        if(!senderId || !message){
-            res.status(400).send({error:"Please provide senderId and message"})
+router.post("/message", async (req, res) => {
+    try {
+        const { conversationId, senderId, message, recieverId = '' } = req.body;
+        if (!senderId || !message) {
+            return res.status(400).send({ error: "Please provide senderId and message" });
         }
-        if(!conversationId){
-            const newConversation= new Conversation({member:[senderId,recieverId]});
+        if (!conversationId) {
+            const newConversation = new Conversation({ members: [senderId, recieverId] });
             await newConversation.save();
-            const newMessage= new Message({conversationId:newConversation._id, senderId, message});
+            const newMessage = new Message({ conversationId: newConversation._id, senderId, message });
             await newMessage.save();
-            res.status(200).send("Message send successfully");
+            return res.status(200).send("Message sent successfully");
         }
-        
-        const newMessage= new Message({conversationId, senderId, message})
+
+        const newMessage = new Message({ conversationId, senderId, message });
         await newMessage.save();
-        res.status(200).send("Message send successfully")
-    }catch(error){
+        return res.status(200).send("Message sent successfully");
+    } catch (error) {
         console.log(error);
-        res.send(500).send({error:"Server error. Please try again."});
+        return res.status(500).send({ error: "Server error. Please try again." });
     }
-})
+});
 
-router.get("/mentor/message/:conversationId", async(req, res)=>{
-    try{
-        const conversationId= req.params.conversationId;
-        const messages= await Message.find({conversationId});
-        const messageData= Promise.all(messages.map(async(msg)=>{
-            const mentor= await Mentor.findById(msg.senderId);
-            return {mentor:{senderId:mentor.senderId, message:mentor.message, fullName:mentor.fullName},message:messages.message} 
-        }
-        ))
-        res.status(200).send({message:"All messages", messageData})
-    }catch(error){
+router.get("/mentor/message/:conversationId", async (req, res) => {
+    try {
+        const conversationId = req.params.conversationId;
+        const messages = await Message.find({ conversationId });
+        const messageData = await Promise.all(messages.map(async (msg) => {
+            const mentor = await Mentor.findById(msg.senderId);
+            return { mentor: { senderId: mentor._id, message: msg.message, fullName: mentor.fullName } };
+        }));
+        res.status(200).send({ message: "All messages", messageData });
+    } catch (error) {
         console.log(error);
-        res.send(500).send({error:"Server error. Please try again."});
+        return res.status(500).send({ error: "Server error. Please try again." });
     }
-})
+});
 
-router.get("/product/message/:conversationId", async(req, res)=>{
-    try{
-        const conversationId= req.params.conversationId;
-        const messages= await Message.find({conversationId});
-        const messageData= Promise.all(messages.map(async(msg)=>{
-            const product= await Product.findById(msg.senderId);
-            return {product:{senderId:product.senderId, fullName:product.fullName},message:messages.message} 
-        }
-        ))
-        res.status(200).send({message:"All messages", messageData})
-    }catch(error){
+router.get("/product/message/:conversationId", async (req, res) => {
+    try {
+        const conversationId = req.params.conversationId;
+        const messages = await Message.find({ conversationId });
+        const messageData = await Promise.all(messages.map(async (msg) => {
+            const product = await Product.findById(msg.senderId);
+            return { product: { senderId: product._id, fullName: product.fullName }, message: msg.message };
+        }));
+        res.status(200).send({ message: "All messages", messageData });
+    } catch (error) {
         console.log(error);
-        res.send(500).send({error:"Server error. Please try again."});
+        return res.status(500).send({ error: "Server error. Please try again." });
     }
-})
+});
 
-router.get("/product/conversation/:userId", async(req, res)=>{
-    try{
+router.get("/product/conversation/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
 
-        const userId= req.params.userId;
+        const conversations = await Conversation.find({ members: { $in: [userId] } });
 
-        const conversation= await Conversation({member:{$in:[userId]}})
-
-        const converstionUserData= Promise.all(conversation.map(async(conv)=>{
-            const recieverId= conv.member.find((mem)=> mem!==userId);
-            const product= await Product.findById(recieverId);
-            return { product: {email:product.email, fullName:product.fullName}, conversationId:conv._id}
+        const conversationUserData = await Promise.all(conversations.map(async (conv) => {
+            const recieverId = conv.members.find((mem) => mem !== userId);
+            const product = await Product.findById(recieverId);
+            return { product: { email: product.email, fullName: product.fullName }, conversationId: conv._id };
         }));
 
+        res.status(200).send({ message: "All conversations", conversationData: conversationUserData });
 
-        res.status(200).send({message:"All conversation", conversationData:converstionUserData})
-
-    }catch(error){
+    } catch (error) {
         console.log(error);
-        res.send(500).send({error:"Server error. Please try again."});
+        return res.status(500).send({ error: "Server error. Please try again." });
     }
-})
+});
 
 module.exports = router;
